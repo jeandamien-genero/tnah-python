cfdbff824b2bf449093f4d356b76ceb2bfc55ed8
Jean-Damien Généro <jdgenero@MacBook-Air-002.local>
2020-01-19 17:23:53 +0100

    correction

--- csv_json_requests_1.py
+++ csv_json_requests_1.py
@@ -1,3 +1,39 @@
+#Correction
+import csv
+import requests
+
+def iiif_csv(ark, csv_name):
+    """ Displays the metada of a BnF's manuscript out of its json file, and write in a .csv a table with pages' informations (number, name, link, width and height).
+    :param ark: BnF's ark id
+    :type ark: str
+    :param csv_name: csv file where the table will be written
+    :type nom_csv: str
+    :return : none
+    """
+    columns = ["Number", "Page name", "Image link", "Width", "Height"]
+    r = requests.get("http://gallica.bnf.fr/iiif/" + ark + "/manifest.json")
+    data = r.json()
+
+    for x in data["metadata"]:
+        print(x["label"] + " " + x["value"])
+
+    with open(csv_name, "w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(columns)
+        number = 0
+        for canvases in data["sequences"][0]["canvases"]:
+            number += 1
+            page_name = canvases["label"]
+            image_link = canvases["images"][0]["resource"]["@id"]
+            width = canvases["width"]
+            height = canvases["height"]
+            csv_writer.writerow([number, page_name, image_link, width, height])
+
+    return None
+
+iiif_csv("ark:/12148/btv1b84259980", "/Users/jdgenero/Desktop/M2_Cours/cours-python-master/data/csv/pages.csv")
+
+#Proposition
 def iiif_csv(ark, nom_csv):
     """ Displays the metada of a BnF's manuscript out of its json file, and write in a .csv a table with pages' informations (number, name, picture'link, hight and width)
     :param ark: BnF's ark id
@@ -22,18 +58,18 @@ def iiif_csv(ark, nom_csv):
     print(metadata)
 
     #3. Making of the table
-    list_column = []
-
-    list_column.append(1)
-    list_column.append(metadata[5]["value"]) #file's name
-    list_column.append(metadata[2]["value"]) #file's image
-    list_column.append(data["sequences"][0]["canvases"][0]["height"]) #height
-    list_column.append(data["sequences"][0]["canvases"][0]["width"]) #width
 
     #4. CSV
     with open (nom_csv, "w") as csv_file :
         writing = csv.writer(csv_file, delimiter=",")
         writing.writerow(colonnes)
-        writing.writerow(list_column)
+        numero = 0
+        for canvas in data["sequences"][0]["canvases"] :
+            numero += 1
+            page_name = canvas["label"]
+            image_link = canvas["images"][0]["ressource"]["@id"]
+            hauteur = canvas["height"]
+            largeur = canvas["width"]
+            csv_writer.writerow([numero, page_name, image_link, hauteur, largeur])
 
 iiif_csv("ark:/12148/btv1b84259980", "/data/csv/pages.csv")
--- ex_csv_json.py
+++ ex_csv_json.py
@@ -8,23 +8,43 @@ import json
 import csv
 
 
+#Correction ex. 1
+with open("data/json/twitter.humanitesnumeriques.json") as json_file :
+    tweets = json.load(json_file)
+for tweet in tweets["statuses"]:
+    print("http://twitter.com/statuses/{0} {1} {2}".format(
+    tweet["id_str"], tweet["user"]["screen_name"],
+    tweet["text"]
+    ))
+
+
+#Ex 2
 def tweeter(index):
     """ Makes a list of a tweet's link, user_id, time and text out of a json file which contain metadata from Twitter.
         :param index: the tweet's index in the statuses list
         :return: a list with the tweet's URL (link), username (user_id), date (time) and content (tweet)
-        :rtype: str
+        :rtype: list
     """
     link = "http://twitter.com/statuses/" + (twitter_file["statuses"][index]["id_str"])
     user_id = twitter_file["statuses"][index]["user"]["screen_name"]
     time = twitter_file["statuses"][index]["created_at"]
     tweet = twitter_file["statuses"][index]["text"]
-    return([link, user_id, time, tweet])
+    return [link, user_id, time, tweet]
+"""
+Notes de correction. Problème de cette fonction : elle n'est pas utilisable avec tous les fichiers. Pour la rentrer universel, il faut ajouter le fichier dans les paramètres.
+Fonction corrigée :
+def affichage_tweet(index, data):
+    lien = "http://twitter.com/statuses/" + \
+           data["statuses"][index]["id_str"]
+    auteur = data["statuses"][index]["user"]["screen_name"]
+    date = data["statuses"][index]["user"]["created_at"]
+    tweet = data["statuses"][index]["text"]
+    return [lien, auteur, date, tweet]
+"""
 
-#Solution 1
 with open("data/csv/tabl_tweet.csv", "w") as tabl_tweet:
-    titlewritter = csv.writer(tabl_tweet, delimiter=",")
-    titlewritter.writerow(["Lien", "Auteur", "Date", "Tweet"]) #
-    
+    titlewritter = csv.writer(tabl_tweet)
+    titlewritter.writerow(["Lien", "Auteur", "Date", "Tweet"])
 with open("data/json/twitter.humanitesnumeriques.json") as json_file :
     twitter_file = json.load(json_file)
     index = 0
@@ -32,21 +52,25 @@ with open("data/json/twitter.humanitesnumeriques.json") as json_file :
         row = tweeter(index)
         index += 1
         with open("data/csv/tabl_tweet.csv", "a") as tabl_tweet :
-            tweetwritter = csv.writer(tabl_tweet, delimiter=",")
+            tweetwritter = csv.writer(tabl_tweet)
             tweetwritter.writerow(row)
 
+#Correction ex. 2
+def affichage_propre(statut):
+    lien = "http://twitter.com/statuses/" + statut["id_str"]
+    auteur = statut["user"]["screen_name"]
+    date = statut["created_at"]
+    tweet = statut["text"]
+    return [lien, auteur, date, tweet] # pas besoin de mettre des paranthèses
 
-#Solution 2
-with open("data/csv/tabl_tweet.csv", "w") as tabl_tweet:
-    spamwritter = csv.writer(tabl_tweet, delimiter=",")
-    spamwritter.writerow(["Lien", "Auteur", "Date", "Tweet"])
 
-with open("data/json/twitter.humanitesnumeriques.json") as json_file :
-    twitter_file = json.load(json_file)
-    nombre = 0
-    while (len(twitter_file["statuses"]) - 1) > nombre:
-        nombre += 1
-        row = tweeter(nombre)
-        with open("data/csv/tabl_tweet.csv", "a") as tabl_tweet :
-            spamwritter = csv.writer(tabl_tweet, delimiter=",")
-            spamwritter.writerow(row)
+with open("data/json/twitter.humanitesnumeriques.json") as json_file:
+    tweeter_f = json.load(json_file)
+
+with open("data/csv/tabl_tweet.csv", mode="w") as csv_file:
+    head_writer = csv.writer(csv_file)
+    head_writer.writerow(["Lien", "Auteur", "Date", "Tweet"])
+
+    for line in tweeter_f["statuses"]:
+        row = affichage_propre(line)
+        head_writer.writerow(row)
